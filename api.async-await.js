1305ea406ea48c7d166cd184d4bee09fc75dfa57
Roman Tuomisto <romantuomisto@gmail.com>
2022-10-28 22:30:25 +0300

    adds async await example

---
+++ api.async-await.js
@@ -0,0 +1,6 @@
+const axios = require('axios');
+
+(async() => {
+    const starwarsHeroes = await axios.get("https://swapi.dev/api/people/1");
+    console.log("people are: ", starwarsHeroes);
+})();
